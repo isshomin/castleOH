5ed11a41ea73e4ceb1b7533bee70d871c639519f
ohwoong2 <ohwoonge@gmail.com>
2024-08-02 12:05:48 +0900

    koko's utter

--- src/step01/hello.java
+++ src/step01/hello.java
@@ -4,6 +4,14 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("상민");
+				System.out.println("상민");
+		System.out.println("상민");
+		System.out.println("상민");
+		System.out.println("상민");
+		System.out.println("상민");
+		System.out.println("상민");
+		System.out.println("상민");
+
 		System.out.println();
 	}
 
